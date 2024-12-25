07f967770947bd4b8f695a8bcbb6c8fb93a3bed4
abd169 <abdalrahmanamer427@gmail.com>
2024-12-25 10:15:12 -0800

    Save current progress before pull

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base:"/my-vue-app/"
-=======
-  base:"Fresh-Cart-"
->>>>>>> 07739d3 (first commit)
+
+  // base:"/my-vue-app/",
+
+  base:"Fresh-Cart-",
+
 })
